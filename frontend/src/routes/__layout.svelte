a00dc32c0d6099f5a7d4edd749ed914bdeb65bb7
IsolatedLun <herojoe210@gmail.com>
2022-07-18 15:53:55 +0300

    scss foundation changes

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<nav class="[ main-navbar ] [ column-split-2 ] [ padding-inline-2 padding-block-1 ]">
+    <h1>Ternary</h1>
+
+    <input type="text" placeholder="Search" />
+
+    <div class="[ nav__user ] [ flex gap-2 ]">
+        <div>
+            <p>IsolatedLun</p>
+            <p>0 honor</p>
+        </div>
+        <img class="[ profile ]" src="" alt="" />
+    </div>
+</nav>
+
+<main class="[ main-container ] [ margin-block-2 gap-2 margin-inline-auto ]" data-grid-collapse>
+    <slot></slot>
+</main>
